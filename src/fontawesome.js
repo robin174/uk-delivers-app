2b7a13fe1a2b5ce802b731c1516e978f0a9ce3ae
Robin Green <robin@designingreen.co.uk>
2020-05-22 18:44:52 +0100

    Font Awesome implementation

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,7 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-
 import { faAdjust } from '@fortawesome/free-solid-svg-icons';
 
-library.add(
-    faAdjust
-);
+library.add(faAdjust);
