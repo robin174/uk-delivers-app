d66cedf03d822423319c1bdc7cdc7c711ec073a1
Robin Green <robin@designingreen.co.uk>
2020-05-22 18:54:27 +0100

    Naming issue

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import AppProviders from './context';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './fontAwesome';
+import './fontawesome';
 
 ReactDOM.render(
   <AppProviders>
