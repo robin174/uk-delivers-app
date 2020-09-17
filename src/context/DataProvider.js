6947ae3ab574d78e7ea10a0967e0ed187949f0d4
Robin Green <robin@designingreen.co.uk>
2020-09-17 12:59:34 +0100

    Sort sorting error

--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -42,8 +42,8 @@ const DataProvider = (props) => {
         .then(data => {      
           const array = data.default.slice(0);
           const sorted = array.sort((a,b) => {
-            const x = a.slug.toLowerCase();
-            const y = b.slug.toLowerCase();
+            const x = a.order;
+            const y = b.order;
             return x < y ? -1 : x > y ? 1 : 0;
           });
           setLocations(sorted);
